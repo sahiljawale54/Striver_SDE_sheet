0837029f8dac503f8c2f35876422d5bf2f8fdb21
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2022-08-02 18:27:01 +0530

    Create sqrt(x).cpp

---
+++ sqrt(x).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long int BS(int x){
+        int s=0;
+        int e=x/2;
+        long long int mid= s+(e-s)/2;
+        long long int ans=-1;
+        
+        while(s<=e){
+            
+            long long int  sq=mid*mid;
+            if(sq==x){
+                return mid;
+            }
+            else if(sq<x){
+                ans= mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+    int mySqrt(int x) {
+        return BS(x);
+    }
+};
