dfea54d7e85fc38fef5cfba90cece2bc7c5c4dad
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2023-07-03 10:00:39 +0530

    Time: 0 ms (100.00%), Space: 7 MB (87.16%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        //obervation
+        
+        // tranppose karo , phir row wise rev
+        
+        int n = mat.size();
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
+                swap(mat[i][j] , mat[j][i]);
+            }
+        }
+        
+        for(int i = 0 ; i < n  ;i++){
+            reverse(mat[i].begin() , mat[i].end());
+        }
+        
+        
+    }
+};
