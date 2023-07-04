e0c49e43481a9edd24c5dcb6ff700db81901e542
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2023-07-04 17:45:31 +0530

    Time: 15 ms (65.03%), Space: 15.7 MB (93.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> res;
+        //optimal 
+        // moore voting for 2 values
         
-        for(int ele : nums) {
-            mp[ele]++;
-        }
-        for(auto it : mp){
-            if(it.second > nums.size() /3) res.push_back(it.first);
-        }
-        return res;
+        int ele1=INT_MIN , ele2=INT_MIN , cnt1 = 0 ,  cnt2 = 0;
         
+        for(int i= 0 ; i< nums.size() ; i++){
+            if(cnt1 == 0 && ele2 != nums[i]){
+                ele1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0 && ele1 != nums[i]){
+                ele2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(ele1 == nums[i]) cnt1++;
+            else if(ele2 == nums[i]) cnt2++;
+            else {
+                cnt1-- ; cnt2--;
+            }
+            
+        }
+        vector<int> ans ;
+        cnt1 = 0 , cnt2 = 0;
+        for(auto ele : nums){
+             if(ele1 == ele) cnt1++;
+              if(ele2 == ele) cnt2++;
+        }
+        int mini = nums.size() /3 +1;
+        if(cnt1>=mini) ans.push_back(ele1);
+        if(cnt2 >= mini) ans.push_back(ele2);
+         return ans;
     }
 };
