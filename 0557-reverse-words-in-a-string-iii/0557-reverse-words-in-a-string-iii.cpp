19cfffab5615e5837c634f1a136a346be733656b
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2023-07-12 19:08:57 +0530

    Time: 15 ms (62.54%), Space: 9.5 MB (91.06%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n = s.size();
-        string ans = "";
+      //string ans = "";
         
         int i =0 , j = 0;
         
         while(i < n){
             //j ko space tk leke ja 
             while(j < n && s[j] != ' ') j++;
-            string w = s.substr(i , j-i);
-            reverse(w.begin() , w.end());
-            if(ans.size() == 0) ans = w;
-            else ans = ans + " " + w;
+            //string w = s.substr(i , j-i);
+            reverse(s.begin() + i , s.begin()+ j);
+           //if(ans.size() == 0) ans = w;
+            //else ans = ans + " " + w;
             i = j+1;
             j++;
         }
         
-        return ans;
+        return s;
     }
 };
