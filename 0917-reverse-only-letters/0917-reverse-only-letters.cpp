acea3577f6895760a90476a88a4796e282e19801
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2023-07-29 10:43:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (88.40%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int n = s.size();
+        int i = 0 , j = n-1;
+        
+        while(i<=j){
+            if(isalpha(s[i])  && isalpha(s[j])){
+                swap(s[i++] , s[j--]);
+            }
+            else if(!isalpha(s[i])) i++;
+            else j--;
+            
+        }
+        return s;
+    }
+};
